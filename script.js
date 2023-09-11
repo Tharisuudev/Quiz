80ed2a955b25041e23e7bf7930c2840fafde04be
thsagacity <thsagacity@gmail.com>
2023-09-10 23:49:06 -0700

    Added JS, HTML, CSS

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const startBtn = document.querySelector(".start-btn");
 const popupInfo = document.querySelector(".popup-info");
 const exitBtn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
+const continueBtn = document.querySelector(".continue-btn");
+const quizSection = document.querySelector(".quiz-section");
+
 startBtn.onclick = () => {
   popupInfo.classList.add("active");
+  main.classList.add("active");
 };
 
 exitBtn.onclick = () => {
   popupInfo.classList.remove("active");
+  main.classList.remove("active");
 };
 
-const main = document.querySelector(".main");
-const continueBtn = document.querySelector(".continue-btn");
-const quizSection = document.querySelector(".quiz-section");
+continueBtn.onclick = () => {
+  quizSection.classList.add("active");
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+};
+
 const quizBox = document.querySelector(".quiz-box");
 const resultBox = document.querySelector(".result-box");
 const tryAgainBtn = document.querySelector(".tryAgain-btn");
