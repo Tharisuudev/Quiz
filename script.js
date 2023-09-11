2bc3bccea39ad4fcf32052e49cfedfb1e849bd6c
Tharisuudev <thalyssouzadev@gmail.com>
2023-09-11 19:24:18 -0400

    fixing JS

--- script.js
+++ script.js
@@ -19,4 +19,32 @@ continueBtn.onclick = () => {
   popupInfo.classList.remove("active");
   // main.classList.remove("active");
   quizBox.classList.add("active");
+
+  showQuestions(0);
 };
+
+let questionCount = 0;
+
+const nextBtn = document.querySelector(".next-btn");
+
+nextBtn.onclick = () => {
+  questionCount++;
+  showQuestions(questionCount);
+};
+
+const optionList = document.querySelector(".option-list");
+
+//getting questions and options from array
+
+function showQuestions(index) {
+  const questionText = document.querySelector(".question-text");
+  questionText.textContent = `${questions[index].numb}. ${questions[index].question}}`;
+
+  let optionTag = `<div class="option"><span>${questions[index].options[0]}/span></div>
+  <div class="option"><span>${questions[index].options[0]}/span></div>
+  <div class="option"><span>${questions[index].options[1]}/span></div>
+  <div class="option"><span>${questions[index].options[2]}/span></div>
+  <div class="option"><span>${questions[index].options[3]}/span></div>`;
+
+  optionList.innerHTML = optionTag;
+}
