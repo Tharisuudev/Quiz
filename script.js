ab3bd3e0b54505f18ef372c014d527757b0c6f5f
Tharisuudev <thalyssouzadev@gmail.com>
2023-09-11 18:35:45 -0400

    Fixed Popup Info

--- script.js
+++ script.js
@@ -1,47 +1,22 @@
 const startBtn = document.querySelector(".start-btn");
 const popupInfo = document.querySelector(".popup-info");
 const exitBtn = document.querySelector(".exit-btn");
-const main = document.querySelector(".main");
+const main = document.querySelector("main");
 const continueBtn = document.querySelector(".continue-btn");
 const quizSection = document.querySelector(".quiz-section");
 const quizBox = document.querySelector(".quiz-box");
 
 startBtn.onclick = () => {
   popupInfo.classList.add("active");
-  main.classList.add("active");
 };
 
 exitBtn.onclick = () => {
   popupInfo.classList.remove("active");
-  main.classList.remove("active");
 };
 
 continueBtn.onclick = () => {
   quizSection.classList.add("active");
   popupInfo.classList.remove("active");
-  main.classList.remove("active");
+  // main.classList.remove("active");
   quizBox.classList.add("active");
-
-  showQuestions(0);
 };
-
-let questionCount = 0;
-
-const nextBtn = document.querySelector(".next-btn");
-
-continueBtn.onclick = () => {
-  questionCount++;
-  showQuestions(questionCount);
-};
-
-const optionlist = document.querySelector(".option-list");
-
-let optionTag = ` <div class="option">
-                <span>${questions}[index].options[0]}</span>
-              </div>`;
-
-//getting questions and options from array
-function showQuestions(index) {
-  const questionText = document.querySelector(".question-text");
-  questionText.textContent = `${questions[index].numb}. ${questions[index].questions}`;
-}
